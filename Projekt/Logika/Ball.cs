402af87211151a9ec8b40c8d7cb9e3ae2ad34ab8
StanislawJ2 <102310719+StanislawJ2@users.noreply.github.com>
2022-04-26 14:41:08 +0200

    Zad1

---
+++ Projekt/Logika/Ball.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logika
+{
+    internal class Ball
+    {
+        public Ball()
+        {
+            Random rnd = new Random();
+            this.x = rnd.Next(10, 991);
+            this.y = rnd.Next(10, 991);
+            while (this.speed_x != 0 || this.speed_y != 0) {
+                this.speed_x = (0.01 + rnd.NextDouble()) * rnd.Next(-1, 1);
+                this.speed_y = (0.01 + rnd.NextDouble()) * rnd.Next(-1, 1);
+            }
+
+            this.size = 10;
+        }
+        double x;
+        double y;
+        double speed_x;
+        double speed_y;
+        readonly double size;
+
+        public double Getx()
+        {
+            return x;
+        }
+
+        public double Gety()
+        {
+            return y;
+        }
+
+        public void BallMovement ()
+        {
+            if((this.x + this.speed_x) > 1000 -this.size)
+            {
+                this.x = 1000-this.size;
+                this.speed_x = this.speed_x * -1;
+                return;
+            }
+            if ((this.y + this.speed_y) > 1000-this.size)
+            {
+                this.y = 1000-this.size;
+                this.speed_y = this.speed_y * -1;
+                return;
+            }
+            if ((this.x + this.speed_x) < 0 + this.size)
+            {
+                this.x = 0 + this.size;
+                this.speed_x = this.speed_x * -1;
+                return;
+            }
+            if ((this.y + this.speed_y) < 0 + this.size)
+            {
+                this.y = 0 + this.size;
+                this.speed_y = this.speed_y * -1;
+                return;
+            }
+            this.x += this.speed_x;
+            this.y += this.speed_y;
+            return;
+        }
+
+    }
+}
