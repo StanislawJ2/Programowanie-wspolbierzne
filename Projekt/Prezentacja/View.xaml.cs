9c71dde66dcffed8418b4b85b734f8ef79179f01
StanislawJ2 <102310719+StanislawJ2@users.noreply.github.com>
2022-04-27 20:37:39 +0200

    Zad1

--- Projekt/Prezentacja/View.xaml.cs
+++ Projekt/Prezentacja/View.xaml.cs
@@ -24,15 +24,5 @@ namespace Prezentacja
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
